f21b086bd13f3ca385fbeb10f82444c34cac9da4
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-25 19:22:23 -0300

    clear code

--- api/src/index.ts
+++ api/src/index.ts
@@ -7,11 +7,6 @@ app.use(express.json()); //middleware que transforma la req.body a un json
 
 const PORT = 3008;
 
-app.get("/ping", (_req, res) => {
-  console.log("Hello World!");
-  res.send("pong");
-});
-
 app.use("/api/items", getItems)
 
 app.listen(PORT, () => {
