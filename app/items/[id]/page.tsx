c20053ddd2a97edbf73ef000891f84721b7e73ea
gabyakd2 <gabrielsal00@hotmail.com>
2023-08-31 16:29:21 -0300

    route /item/id in progress

---
+++ app/items/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageItemId() {
+  return (
+    <div>PageItemId</div>
+  )
+}
+
+export default PageItemId
---
+++ app/items/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageItems() {
+  return (
+    <div>PageItems</div>
+  )
+}
+
+export default PageItems
