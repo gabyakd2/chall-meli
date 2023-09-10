a719764ea2b5e5b2b0e4b06798bb515dac515ed1
gabyakd2 <gabrielsal00@hotmail.com>
2023-09-10 10:54:35 -0300

    route by id ready

--- app/items/components/CardList/CardList.tsx
+++ app/items/components/CardList/CardList.tsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import CardItem from '../CardItem/CardItem'
-import { DataItem, Result } from '../../model/dataProp'
+import React from "react";
+import CardItem from "../CardItem/CardItem";
+import { DataItem, Result } from "../../model/dataProp";
+import Link from "next/link";
 
 interface Props {
-  data: DataItem
+  data: DataItem;
 }
 
-function CardList({data}: Props) {
+function CardList({ data }: Props) {
   // console.log(data)
   return (
     <div className="flex items-center justify-center flex-col">
-      {
-        data && data.results.map((product: Result) => (
-          <CardItem 
-            image={product.thumbnail}
-            price={product.price}
-            title={product.title}
-            location={product.address.state_name}
-          />
-        ))
-      }
+      {data &&
+        data.results.map((product: Result) => (
+          <Link href={`/items/${product.id}`} key={product.id}>
+            <CardItem
+              image={product.thumbnail}
+              price={product.price}
+              title={product.title}
+              location={product.address.state_name}
+            />
+          </Link>
+        ))}
     </div>
-  )
+  );
 }
 
-export default CardList
+export default CardList;
