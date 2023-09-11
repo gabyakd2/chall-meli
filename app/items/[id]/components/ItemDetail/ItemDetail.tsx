6aff3227ddc33429d61b9d005beb49a543c4fbf3
gabyakd2 <gabrielsal00@hotmail.com>
2023-09-11 18:37:18 -0300

    panel description ready

--- app/items/[id]/components/ItemDetail/ItemDetail.tsx
+++ app/items/[id]/components/ItemDetail/ItemDetail.tsx
@@ -1,12 +1,32 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-function ItemDetail({data}: any) {
+function ItemDetail({ data }: any) {
   return (
-    <div>
-      <Image src='hola' alt='foto'/>
+    <div className="flex flex-col justify-center items-center">
+      <div className="flex w-full justify-evenly">
+        <Image
+          src={data?.pictures[0]?.url}
+          alt="foto"
+          width="400"
+          height="400"
+        />
+        <div className="flex flex-col">
+          <p>Status</p>
+          <p>Titulo</p>
+          <p>Precio</p>
+          <button>comprar</button>
+        </div>
+      </div>
+      <p>Titulo Descripcion</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        fugit quis, possimus eius nemo sequi dicta quo voluptatibus voluptatem,
+        nobis ducimus ex iusto beatae ipsa recusandae doloremque corporis odit
+        aliquid!
+      </p>
     </div>
-  )
+  );
 }
 
-export default ItemDetail
+export default ItemDetail;
