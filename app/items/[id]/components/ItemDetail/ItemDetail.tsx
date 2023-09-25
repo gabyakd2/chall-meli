7864a2422aacb03af84743d739540aedbc48956f
gabyakd2 <gabrielsal00@hotmail.com>
2023-09-25 18:33:08 -0300

    item detail responsive

--- app/items/[id]/components/ItemDetail/ItemDetail.tsx
+++ app/items/[id]/components/ItemDetail/ItemDetail.tsx
@@ -19,35 +19,37 @@ function ItemDetail({ data }: Props) {
   }));
   return (
     <div className="flex flex-col justify-center items-center w-full">
-      <div className="flex w-full justify-between">
-        <Image
-          src={data?.pictures[0]?.url}
-          alt="Image Item"
-          width="400"
-          height="400"
-        />
-        <div className="flex flex-col w-[300px]">
-          <p>{data?.sold_quantity} vendidos</p>
-          <p className="font-bold text-xl">{data?.title}</p>
-          <div className="flex mb-9">
-            <p className="font-bold text-2xl">$ {data?.price}</p>00
-          </div>
-          <Button color="primary" variant="shadow">
-            Comprar
-          </Button>
-        </div>
+  <div className="flex flex-col md:flex-row w-full justify-between">
+    <Image
+      src={data?.pictures[0]?.url}
+      alt="Image Item"
+      width="400"
+      height="400"
+      className="md:w-1/2" // Tamaño de la imagen en pantallas grandes
+    />
+    <div className="flex flex-col w-full md:w-1/2 md:ml-6"> {/* Ancho completo en pantallas pequeñas */}
+      <p>{data?.sold_quantity} vendidos</p>
+      <p className="font-bold text-xl">{data?.title}</p>
+      <div className="flex mb-3 md:mb-9"> {/* Separación menor en pantallas grandes */}
+        <p className="font-bold text-2xl">$ {data?.price}</p>
+        <p className="text-default-400">00</p>
       </div>
-      <div className="ml-10 mt-14 mx-auto w-[800px]">
-        <p className="font-bold text-3xl mb-6">Descripción del producto</p>
-        <div className="flex flex-wrap nowrap mb-10">
-          {descriptionItem?.map(({ name, value }: Description) => (
-            <p className="text-default-400">
-              <span className="font-bold">{name}</span>: {value},{" "}
-            </p>
-          ))}
-        </div>
-      </div>
+      <Button color="primary" variant="shadow">
+        Comprar
+      </Button>
     </div>
+  </div>
+  <div className="ml-5 mt-5 md:mt-14 mx-auto w-full md:w-[800px]"> {/* Ancho completo en pantallas pequeñas */}
+    <p className="font-bold text-3xl mb-6">Descripción del producto</p>
+    <div className="flex flex-wrap nowrap mb-10">
+      {descriptionItem?.map(({ name, value }: Description) => (
+        <p className="text-default-400" key={name}>
+          <span className="font-bold">{name}</span>: {value},{" "}
+        </p>
+      ))}
+    </div>
+  </div>
+</div>
   );
 }
 
